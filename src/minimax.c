81e1772d62667afd84930af762c30133fe077ad0
Alessandro <aless.pep@hotmail.fr>
2021-01-27 19:13:06 +0100

    Bug corrigé

--- src/minimax.c
+++ src/minimax.c
@@ -2,6 +2,47 @@
 #include "position.h"
 #include "functions.h"
 
+Position playMove(Position position, int cell) {
+	int seeds;
+
+	// On récupère le nombre de graines à la case choisie et on egrène
+	seeds = plantSeeds(&position, cell);
+
+	// On regarde s'il y a une prise
+	takeSeeds(&position, cell, seeds);
+
+	// On regarde si le joueur qui n'était pas en train de jouer est affamé suite au coup
+	if (isOpponentStarved(position)) {
+		// On ajoute les graines restantes au total du joueur qui vient de jouer le coup
+		takeAllSeeds(&position);
+	}
+
+	// S'il reste moins de 48 graines, on fusionne les cellules
+	if (position.seeds_total < 48 && !position.merged) {
+		merge(&position);
+	}
+
+	return position;
+}
+
+int max(a, b) {
+	if (a > b) {
+		return a;
+	}
+	else {
+		return b;
+	}
+}
+
+int min(a, b) {
+	if (a < b) {
+		return a;
+	}
+	else {
+		return b;
+	}
+}
+
 int minimax(Position position, int depth) {
 	if (depth == 0 || gameOver(position)) {
 		return evaluation(position);
@@ -56,45 +97,4 @@ int minimax(Position position, int depth) {
 
 int evaluation(Position position) {
 	return position.seeds_computer - position.seeds_player;
-}
-
-Position playMove(Position position, int cell) {
-    int seeds;
-
-    // On récupère le nombre de graines à la case choisie et on egrène
-    seeds = plantSeeds(&position, cell);
-
-    // On regarde s'il y a une prise
-    takeSeeds(&position, cell, seeds);
-
-	// On regarde si le joueur qui n'était pas en train de jouer est affamé suite au coup
-	if (isOpponentStarved(position)) {
-		// On ajoute les graines restantes au total du joueur qui vient de jouer le coup
-		takeAllSeeds(&position);
-	}
-
-	// S'il reste moins de 48 graines, on fusionne les cellules
-	if (position.seeds_total < 48 && !position.merged) {
-		merge(&position);
-	}
-
-	return position;
-}
-
-int max(a, b) {
-	if (a > b) {
-		return a;
-	}
-	else {
-		return b;
-	}
-}
-
-int min(a, b) {
-	if (a < b) {
-		return a;
-	}
-	else {
-		return b;
-	}
 }
